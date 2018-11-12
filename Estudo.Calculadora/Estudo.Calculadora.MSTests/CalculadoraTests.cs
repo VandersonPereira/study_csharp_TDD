7a8fa10f895e944ba38bacf1f6954115bb1045cb
Vanderson Pereira <vandoap@hotmail.com>
2018-11-12 10:10:56 -0200

    MSTests
    Create tests using MSTests.

---
+++ Estudo.Calculadora/Estudo.Calculadora.MSTests/CalculadoraTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Estudo.Calculadora.MSTests
+{
+    [TestClass]
+    public class CalculadoraTests
+    {
+        #region Inicio dos testes
+        Util.Calculadora _calc;
+
+        // Esse decorador, é usado para preparar o ambiente para os testes. Poderia sera aberta a conexão com o banco aqui por exemplo.
+        [TestInitialize]
+        public void SetUp()
+        {
+            _calc = new Util.Calculadora();
+        }
+        #endregion
+
+        #region Testes
+        [TestMethod]
+        public void TestSomar()
+        {
+            Assert.AreEqual(4, _calc.Somar(2, 2));
+        }
+
+        [TestMethod]
+        public void TestSubtrair()
+        {
+            Assert.AreEqual(0, _calc.Subtrair(2, 2));
+        }
+
+        // Testes que esperam excessões.
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestSomarComException()
+        {
+            Util.Calculadora calcWithException = new Util.Calculadora(2);
+            calcWithException.Somar(1, 1);
+        }
+
+        // Testes que ainda não foram pensados, podem ser escritos e ignorados, assim eles não somem do radar.
+        [TestMethod]
+        [Ignore]
+        public void QualquerMetodo()
+        {
+            
+        }
+        #endregion
+
+        #region Fim de testes
+        // Depois de todos os testes, pode-se limpar os objetos, ou realizar alguma tratativa final.
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _calc = null;
+        }
+        #endregion
+    }
+}
---
+++ Estudo.Calculadora/Estudo.Calculadora.MSTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Estudo.Calculadora.MSTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Estudo.Calculadora.MSTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93f1e317-66e7-4a1e-a71e-096e5856e05b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
